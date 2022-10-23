07c0b01c25c41a9b271743a06bdff7927240bcc1
Denmar E <70527795+getll@users.noreply.github.com>
2022-10-23 00:25:56 -0400

    updated page 1 formatting

--- phase-2/multipage-dash-app-main/pages/page1.py
+++ phase-2/multipage-dash-app-main/pages/page1.py
@@ -15,22 +15,45 @@ GPIO.setmode(GPIO.BCM)
 buzzer=19
 GPIO.setup(buzzer,GPIO.OUT)                                                         
 
+#Set fan pin
+fan=21
+GPIO.setup(fan,GPIO.OUT) 
 
+#bool for fan and email sent
+fanIsOn=False
+emailSent=False
+
+box_style_dict = {
+    'width': '45%',
+    'border': '3px solid gray',
+    'margin': '2.5%',
+    'height': '400px'
+}
 
 layout = html.Div([
-
-    html.I(id='lightbulb',
-    className="bi bi-lightbulb",
-    style={'font-size': '10rem','width':'500px',
-    'padding': '50px'}),
-    
-    daq.BooleanSwitch(
-        id='light-switch',
-        on=True,
-        color='#00EA64',
-        style={'transform': 'scaleX(1.25) scaleY(1.25)'}
-    ),
     html.Div([
+        html.Div([
+            html.I(id='lightbulb',
+            className="bi bi-lightbulb",
+            style={'font-size': '10rem','width':'500px','padding-top': '70px'}),
+            
+            daq.BooleanSwitch(
+                id='light-switch',
+                on=True,
+                color='#00EA64',
+                style={'transform': 'scaleX(1.25) scaleY(1.25)'}
+            ),
+            
+        ],
+        style = {'float': 'left', 'width' : '100%', 'padding-top' : '50px'}
+        ),
+    ], 
+            
+        style = box_style_dict | {'float': 'left'}
+    ),
+        
+    html.Div([
+        html.Div([
         dcc.Interval(id='interval',interval=5 *1000, n_intervals=0),
         daq.Gauge(
             id='temperature',
@@ -40,22 +63,70 @@ layout = html.Div([
             label='Temperature',
             max=40,
             min=-40,
+            style={'transform': 'scaleX(0.80) scaleY(0.80)'}
         ),
-        daq.Thermometer(
-            id='humidity',
-            color="orange",
-            value=0,
-            scale={'start':0,'interval':5},
-            label='Humidity',
-            max=100,
-            min=-0,
-        )
-    ])
+        ], 
+        
+        style = {'float':'left', 'width': '33%','padding-top': '50px'}
+                 
+        ),
+        
+        html.Div([
+        html.I(id='fan',
+        className="bi bi-slash-circle",
+        style={'font-size': '10rem','width':'30px'}),
+        
+        daq.BooleanSwitch(
+            id='fan-switch',
+            on=fanIsOn,
+            color='#00EA64',
+            style={'transform': 'scaleX(1.25) scaleY(1.25)'}
+        ),
+        
+        ], 
+            
+        style = {'float':'left', 'width': '33%','padding-top': '50px'}
+        ),
+        
+        
+        
+        html.Div([
+            daq.Thermometer(
+                id='humidity',
+                color="orange",
+                value=0,
+                scale={'start':0,'interval':5},
+                label='Humidity',
+                max=100,
+                min=-0,
+            )
+        ], 
+        
+        style = {'float':'right', 'width': '33%','padding-top': '50px'}
+        ),
+    ], 
+    
+    style = box_style_dict| {'float': 'right'}
+    ),
+    
+    html.Div([
+    ], 
+    
+    style = box_style_dict | {'float': 'left','clear' : 'left'}
+    ),
+    
+    html.Div([
+    ], 
+    
+    style = box_style_dict | {'float': 'right','clear' : 'right'}
+    )
+    
     ],
     
-    style={'text-align': 'center'}
-
-
+    style={
+        'text-align': 'center',
+        'padding': '25px'
+    }
 )
     
     
