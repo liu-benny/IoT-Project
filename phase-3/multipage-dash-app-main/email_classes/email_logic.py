902b99a3e06bc1212832539697c1b3e5f6e3e69e
getll <ermitano.den@gmail.com>
2022-11-22 23:38:38 -0500

    added db class

---
+++ phase-3/multipage-dash-app-main/email_classes/email_logic.py
@@ -0,0 +1,92 @@
+import email
+import imaplib
+import smtplib
+
+class EmailController:
+    def __init__(self, server, user, password):
+        self.server = server
+        self.user = user
+        self.password = password
+        self.sent = False
+        self.received = False
+
+    def send_email(self, msg):
+        message =  """From: <{sndr}>
+To: Smarthome User <{rcv}>
+Subject: Message sent from Python
+
+{ctnt}""".format(sndr=self.user, rcv=self.user, ctnt=msg)
+
+        try:
+            # sending email
+            smtpObj = smtplib.SMTP(self.server)
+            
+            # sender and receiver are same email, but email message contains <Smarthome User> as sender
+            smtpObj.sendmail(self.user, self.user, message)
+            print (f"Message '{msg}' sent to {self.user}")
+            
+            self.sent = True
+            
+        except smtplib.SMTPException:
+            print ("Could not send email")
+        
+    def check_email(self, filter):
+        
+        # connect to the server and go to its inbox
+        inbox = imaplib.IMAP4_SSL(self.server)
+        inbox.login(self.user, self.password)
+        inbox.select("inbox")
+
+        # get emails
+        status, data = inbox.search(None, "ALL")
+
+        # splitting data blocks
+        mail_ids = []
+        for block in data:
+            mail_ids += block.split()
+
+        # get email and content
+        
+        i = mail_ids.pop()
+        
+        status, data = inbox.fetch(i, "(RFC822)")
+        
+        for response_part in data:
+            if isinstance(response_part, tuple):
+                recieved_message = email.message_from_bytes(response_part[1])
+                
+                # checking sender and receiver
+                mail_sender = recieved_message["from"]
+                mail_receiver = recieved_message["to"]
+                
+                # filtering for specified sender
+                if self.user in mail_sender:
+                    
+                    # checking subject, might need to filter
+                    mail_subject = recieved_message["subject"]
+                    
+                    # checking content 
+                    if recieved_message.is_multipart():
+                        mail_content = ''
+
+                        for part in recieved_message.get_payload():
+                            if part.get_content_type() == "text/plain":
+                                mail_content += part.get_payload()
+                    else:
+                        mail_content = recieved_message.get_payload()
+                    
+                    if (filter in mail_content):
+                        # print mail
+                        print(f"From: {mail_sender}")
+                        print(f"To: {mail_receiver}")
+                        print(f"Subject: {mail_subject}")
+                        print(f"Content: {mail_content}")
+                        
+                        self.received = True
+                        
+                        return True
+                    else:
+                        return False
+    def test(self):
+        print(self.sent)
+        print(self.received)
---
+++ phase-4/multipage-dash-app-main/db/db_class.py
@@ -0,0 +1,92 @@
+import mysql.connector
+
+class DbConnector:
+    def __init__(self, default_admin_card):
+        self.mydb = mysql.connector.connect(
+          host="localhost",
+          user="root",
+          password="root"
+        )
+
+        self.mycursor = self.mydb.cursor()
+
+        self.mycursor.execute("SHOW DATABASES")
+
+        db_exists = False
+        for x in self.mycursor:
+            if "smarthome_db" in x:
+                db_exists = True
+
+        if (not db_exists):
+            self.mycursor.execute("CREATE DATABASE smarthome_db")
+            self.mycursor.execute("USE smarthome_db")
+            self.mycursor.execute('''CREATE TABLE users (
+UserID VARCHAR(255), 
+Name VARCHAR(255),
+TempThreshold INT,
+HumidityThreshold INT,
+LightThreshold INT)''')
+            self.insertUser(default_admin_card, "admin", 22, 80, 500)
+            self.changeUser(default_admin_card)
+        
+        else:
+            self.mycursor.execute("USE smarthome_db")
+            
+        self.insertUser(default_admin_card, "admin", 22, 80, 500)
+        self.readAll()
+        
+        
+        
+    def insertUser(self, user_id, name, temp_threshold, humidity_threshold, light_threshold):
+        sql = "INSERT INTO users (UserID, Name, TempThreshold, HumidityThreshold, LightThreshold) VALUES (%s, %s, %s, %s, %s)"
+        values = (user_id, name, temp_threshold, humidity_threshold, light_threshold)
+        self.mycursor.execute(sql, values)
+
+        self.mydb.commit()
+    
+    def updateTempThreshold(self, temp_threshold):
+        sql = "UPDATE users SET TempThreshold = %d WHERE UserId = %s"
+        values = (temp_threshold, user_id)
+        self.mycursor.execute(sql, values)
+        
+        self.mydb.commit()
+    
+    def updateHumidityThreshold(self, humidity_threshold):
+        sql = "UPDATE users SET HumidityThreshold = %d WHERE UserId = %s"
+        values = (humidity_threshold, user_id)
+        self.mycursor.execute(sql, values)
+        
+        self.mydb.commit()
+    
+    def updateLightThreshold(self, light_threshold):
+        sql = "UPDATE users SET LightThreshold = %d WHERE UserId = %s"
+        values = (light_threshold, user_id)
+        self.mycursor.execute(sql, values)
+        
+        self.mydb.commit()
+    
+    def changeUser(self, user_id):
+        sql = "SELECT * FROM users WHERE UserId='" + user_id + "'"
+
+        self.mycursor.execute(sql)
+        myresult = self.mycursor.fetchall()
+
+        for row in myresult:
+            self.current_user_id = row[0]
+            self.current_name = row[1]
+            self.current_temp_threshold = row[2]
+            self.current_humidity_threshold = row[3]
+            self.current_light_threshold = row[4]
+    
+    def readAll(self):
+        sql = "SELECT * FROM users"
+
+        self.mycursor.execute(sql)
+        myresult = self.mycursor.fetchall()
+
+        for row in myresult:
+            print(row[0])
+            print(row[1])
+            print(row[2])
+            print(row[3])
+            print(row[4])
